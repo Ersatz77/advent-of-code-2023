aff157dcd5431983733f6ada5393ce4c4e40cbf5
Ersatz <cm12mc@gmail.com>
2023-12-05 23:32:29 -0800

    Added quadratic formula function

--- src/utility/math.h
+++ src/utility/math.h
@@ -6,6 +6,7 @@
 
 #include <cmath>
 #include <concepts>
+#include <utility>
 
 namespace aoc
 {
@@ -67,6 +68,24 @@ namespace aoc
         }
 
         return result;
+    }
+
+    std::pair<double, double> quadratic(const double a, const double b, const double c)
+    {
+        double determinant = (b * b) - (4.0 * a * c);
+        double root_1 = (-b + std::sqrt(determinant)) / (2 * a);
+        double root_2 = (-b - std::sqrt(determinant)) / (2 * a);
+
+        return { root_1, root_2 };
+    }
+
+    std::pair<float, float> quadratic(const float a, const float b, const float c)
+    {
+        float determinant = (b * b) - (4.0 * a * c);
+        float root_1 = (-b + std::sqrtf(determinant)) / (2 * a);
+        float root_2 = (-b - std::sqrtf(determinant)) / (2 * a);
+
+        return { root_1, root_2 };
     }
 
 } // aoc
