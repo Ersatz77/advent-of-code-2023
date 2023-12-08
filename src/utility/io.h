8baa31ef9e76d94304a7d0d8f2d4d253287ea6ad
Ersatz <cm12mc@gmail.com>
2023-12-07 23:40:16 -0800

    Added a function for skipping lines

--- src/utility/io.h
+++ src/utility/io.h
@@ -3,12 +3,16 @@
 
 #include <filesystem>
 #include <fstream>
+#include <iostream>
 
 namespace aoc
 {
     // Simplifies the opening of a file by hiding away common checks.
     // If the file can't be opened, an exception will be thrown.
     std::ifstream open_file(const std::filesystem::path& path);
+
+    // Skips a line by reading all characters until `\n`
+    void skip_line(std::istream& stream);
 
 } // aoc
 
