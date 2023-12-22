d45a310f086ec6738ba36d5028a5cb0338254316
Ersatz <cm12mc@gmail.com>
2023-12-22 03:27:02 -0800

    Added a way to get every point between two points

--- src/utility/math.h
+++ src/utility/math.h
@@ -4,9 +4,11 @@
 #include "utility/utility.h"
 #include "utility/vec.h"
 
+#include <algorithm>
 #include <cmath>
 #include <concepts>
 #include <utility>
+#include <vector>
 
 namespace aoc
 {
@@ -32,6 +34,72 @@ namespace aoc
     T manhattan_distance(const Vec3<T>& vec1, const Vec3<T>& vec2)
     {
         return std::abs(vec2.x - vec1.x) + std::abs(vec2.y - vec1.y) + std::abs(vec2.z - vec1.z);
+    }
+
+    template<Number T>
+    std::vector<Vec2<T>> cartesian_product(const T x1, const T y1, const T x2, const T y2)
+    {
+        std::vector<Vec2<T>> res;
+        for (T x = std::min(x1, x2); x <= std::max(x1, x2); ++x)
+        {
+            for (T y = std::min(y1, y2); y <= std::max(y1, y2); ++y)
+            {
+                res.emplace_back(x, y);
+            }
+        }
+
+        return res;
+    }
+
+    template<Number T>
+    std::vector<Vec2<T>> cartesian_product(const Vec2<T>& vec1, const Vec2<T>& vec2)
+    {
+        std::vector<Vec2<T>> res;
+        for (T x = std::min(vec1.x, vec2.x); x <= std::max(vec1.x, vec2.x); ++x)
+        {
+            for (T y = std::min(vec1.y, vec2.y); y <= std::max(vec1.y, vec2.y); ++y)
+            {
+                res.emplace_back(x, y);
+            }
+        }
+
+        return res;
+    }
+
+    template<Number T>
+    std::vector<Vec3<T>> cartesian_product(const T x1, const T y1, const T z1, const T x2, const T y2, const T z2)
+    {
+        std::vector<Vec3<T>> res;
+        for (T x = std::min(x1, x2); x <= std::max(x1, x2); ++x)
+        {
+            for (T y = std::min(y1, y2); y <= std::max(y1, y2); ++y)
+            {
+                for (T z = std::min(z1, z2); z <= std::max(z1, z2); ++z)
+                {
+                    res.emplace_back(x, y, z);
+                }
+            }
+        }
+
+        return res;
+    }
+
+    template<Number T>
+    std::vector<Vec3<T>> cartesian_product(const Vec3<T>& vec1, const Vec3<T>& vec2)
+    {
+        std::vector<Vec3<T>> res;
+        for (T x = std::min(vec1.x, vec2.x); x <= std::max(vec1.x, vec2.x); ++x)
+        {
+            for (T y = std::min(vec1.y, vec2.y); y <= std::max(vec1.y, vec2.y); ++y)
+            {
+                for (T z = std::min(vec1.z, vec2.z); z <= std::max(vec1.z, vec2.z); ++z)
+                {
+                    res.emplace_back(x, y, z);
+                }
+            }
+        }
+
+        return res;
     }
 
     template<std::integral T>
